388332635c6d52c9a8493406816c344d536c3675
Guoliang Cao <gcao@vonage.com>
2010-05-13 15:23:57 -0400

    Around method Notes on execution order when before/after/around are combined

--- lib/aspect4r/around.rb
+++ lib/aspect4r/around.rb
@@ -9,22 +9,22 @@ module Aspect4r
 
     module ClassMethods
       def around_method *methods, &block
-        around_method = methods.pop unless block_given?
+        if block_given?
+          around_method = Aspect4r::Helper.find_available_method_name self, "a4r_around_"
+          define_method around_method, &block
+        else
+          around_method = methods.pop
+        end
 
         methods.each do |method|
-          new_method = Aspect4r::Helper.find_available_method_name self, "#{method}_"
-          alias_method new_method, method
-        
-          if block_given?
-            new_around_method = Aspect4r::Helper.find_available_method_name self, "#{method}_around_"
-            define_method new_around_method, &block
-          else
-            new_around_method = around_method
-          end
-
-          define_method method do |*args|
-            send new_around_method, *([new_method] + args)
-          end
+          method = method.to_sym
+          
+          Aspect4r::Helper.backup_original_method self, method
+          
+          self.a4r_definitions[method] ||= []
+          self.a4r_definitions[method] << Aspect4r::Definition.around(method, around_method, options)
+          
+          Aspect4r::Helper.create_method self, method, self.a4r_definitions[method]
         end
       end
     end
