6fe6cc86ed1a30d5f86c82ed64d7e27dea5446f5
Guoliang Cao <gcao@vonage.com>
2010-06-11 11:18:42 -0400

    Example for advices on class method

---
+++ examples/advices_on_class_method_example.rb
@@ -0,0 +1,40 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'aspect4r'
+
+class A
+  class << self
+    include Aspect4r
+    
+    around :test do |proxy, input|
+      puts 'around test (before)'
+      result = a4r_invoke proxy, input
+      puts 'around test (after)'
+      result
+    end
+    
+    before :test do |input|
+      puts 'before test'
+    end
+    
+    after :test do |result, input|
+      puts 'after test'
+      result
+    end
+    
+    def test input
+      puts 'test'
+      input
+    end
+  end
+end
+
+puts "Example 1:"
+puts A.test(1)
+# ==== Output ====
+# before test
+# around test (before)
+# test
+# around test (after)
+# after test
+# 1
