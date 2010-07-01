583f77d146152d9ab1b0f367bb465202e61e145e
Guoliang Cao <gcao@vonage.com>
2010-07-01 17:01:13 -0400

    Update example output

--- examples/debugger_example.rb
+++ examples/debugger_example.rb
@@ -38,25 +38,25 @@ Aspect4r.debug_end
 
 # ==== Output ====
 # a4r A#test: * Enabled debug mode for A#test
-# a4r A#test: * method 'test' is created
-# a4r A#test: * advice 0 is created with options {} and a block whose arity is 2
-# a4r A#test: * advice 1 is created with options {} and method 'do_something'
-# a4r A#test: * advice 2 is created with options {} and a block whose arity is 2
+# a4r A#test: * method "test" is created
+# a4r A#test: * advice0 <AROUND> is created with options {} and a block whose arity is 2
+# a4r A#test: * advice1 <BEFORE> is created with options {} and method "do_something"
+# a4r A#test: * advice2 <AFTER> is created with options {} and a block whose arity is 2
 # Example 1:
 # a4r A#test: Execution begins with arguments: 1
-# a4r A#test: Invoke advice 1 with arguments: 1
+# a4r A#test: advice1 is invoked with arguments: 1
 # before test
-# a4r A#test: advice 1 returns nil
-# a4r A#test: Invoke advice 0 with arguments: <???>, 1
+# a4r A#test: advice1 returns nil
+# a4r A#test: advice0 is invoked with arguments: #<UnboundMethod: A#test>, 1
 # around test 1
-# a4r A#test: Invoke wrapped method with arguments 1
+# a4r A#test: "test" is invoked with arguments: 1
 # test
-# a4r A#test: Wrapped method returns 1
+# a4r A#test: "test" returns 1
 # around test 2
-# a4r A#test: advice 0 returns 1
-# a4r A#test: Invoke advice 2 with arguments: 1, 1
+# a4r A#test: advice0 returns 1
+# a4r A#test: advice2 is invoked with arguments: 1, 1
 # after test
-# a4r A#test: advice 2 returns 1
+# a4r A#test: advice2 returns 1
 # a4r A#test: Execution finishes with result: 1
 # 1
 # a4r A#test: * Disabled debug mode for A#test
