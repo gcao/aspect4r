fbeb33589c2060f6b49b8462105d3aa9442485f7
Guoliang Cao <gcao99@gmail.com>
2011-04-22 16:52:24 -0400

    Compare strings instead of using regular expressions in method_added hooks

--- lib/aspect4r/base.rb
+++ lib/aspect4r/base.rb
@@ -28,7 +28,7 @@ module Aspect4r
       def method_added method
         super method
     
-        return if method.to_s =~ /a4r/
+        return if method.to_s[0..2] == /a4r/
 
         # save unbound method and create new method
         if method_advices = a4r_data[method] and not Aspect4r::Helper.creating_method?
@@ -40,7 +40,7 @@ module Aspect4r
       def singleton_method_added method
         super method
     
-        return if method.to_s =~ /a4r/
+        return if method.to_s[0..2] == /a4r/
 
         eigen_class = class << self; self; end
 
